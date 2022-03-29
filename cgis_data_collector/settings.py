93aa35ec037339e2c90ab4662494ba48d198ecdf
AnimeshN <animesh.nauti@gmail.com>
2022-03-29 22:00:42 +0530

    decouple

--- cgis_data_collector/settings.py
+++ cgis_data_collector/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SK')
+SECRET_KEY = config('SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('AH1')]
+ALLOWED_HOSTS = [config('AH1')]
 
 
 # Application definition
